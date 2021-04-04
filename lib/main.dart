33f1c5c637fc0cb05327f10a8ad36f80bb9a278b
agnoam <agnoam.aghai.ooo.123@gmail.com>
2021-04-04 14:14:06 +0300

    added glb file

--- lib/main.dart
+++ lib/main.dart
@@ -60,9 +60,12 @@ class _RemoteObjectState extends State<RemoteObject> {
     if (image != null) {
       final ArCoreReferenceNode node = ArCoreReferenceNode(
         name: "Toucano",
-        objectUrl: "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf",
-        scale: vector.Vector3(0.1, 0.1, 0.1)
+        objectUrl: 
+        'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf'/* "https://raw.githubusercontent.com/agnoam/ar_flutter_poc/master/assets/objects/untitled.gltf" */
+        // scale: vector.Vector3(0.1, 0.1, 0.1)
       );
+
+      arCoreController.removeNodeWithIndex(0);
       arCoreController.addArCoreNodeToAugmentedImage(node, image.index);
     }
   }
